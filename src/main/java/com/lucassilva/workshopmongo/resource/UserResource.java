d798c6cde42267ba186d9fe385f4410bebe9c65d
lucasscardoso <gajeel.red.fox@hotmail.com>
2025-04-16 22:23:06 -0300

    connected to mondb and repository/service created

--- src/main/java/com/lucassilva/workshopmongo/resource/UserResource.java
+++ src/main/java/com/lucassilva/workshopmongo/resource/UserResource.java
@@ -1,26 +1,26 @@
 package com.lucassilva.workshopmongo.resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lucassilva.workshopmongo.domain.User;
+import com.lucassilva.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		User lucas = new User ("1", "lucas silva", "lucas@gmail.com");
-		User yuri = new User ("2", "Yuri cardoso", "yuri@gmail.com");
-		List list = new ArrayList<>();
-		list.addAll(Arrays.asList(lucas,yuri));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
