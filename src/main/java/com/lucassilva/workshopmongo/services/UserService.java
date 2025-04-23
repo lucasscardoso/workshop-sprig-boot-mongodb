2c68b04cc9ba26da4f1b633ab1a0e013bb9435fc
lucasscardoso <gajeel.red.fox@hotmail.com>
2025-04-22 23:02:11 -0300

    getting a user by id and exception handling

--- src/main/java/com/lucassilva/workshopmongo/services/UserService.java
+++ src/main/java/com/lucassilva/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.lucassilva.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lucassilva.workshopmongo.domain.User;
 import com.lucassilva.workshopmongo.repository.UserRepository;
+import com.lucassilva.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,10 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findById(String id) {
+	    Optional<User> user = repo.findById(id);
+	    return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+
 }
