54d80d53ffc81275aa568ac093d56364d897eb1b
lucasscardoso <gajeel.red.fox@hotmail.com>
2025-05-02 15:18:30 -0300

    getting posts by id

---
+++ src/main/java/com/lucassilva/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.lucassilva.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucassilva.workshopmongo.domain.Post;
+import com.lucassilva.workshopmongo.repository.PostRepository;
+import com.lucassilva.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	
+	public Post findById(String id) {
+	    Optional<Post> post = repo.findById(id);
+	    return post.orElseThrow(() -> new ObjectNotFoundException("Post não encontrado"));
+	}
+	
+	
+
+}
